e99d552307abeb4b6162af84a1d939f1cbfcf286
Gabriel <gc.novaes@outlook.com>
2022-11-02 10:29:44 -0300

    adiciona base de arquivos e struct de livros

---
+++ src/books.h
@@ -0,0 +1,13 @@
+#include <string>
+
+struct Book{
+    std::string disciplina;
+    std::string titulo;
+    std::string autir;
+    int edicao;
+    std::string cidade;
+    std::string editora;
+    int ano;
+    std::string isbn;
+    bool basica; // bibliografia basica
+};
---
+++ src/file_manager.cpp
@@ -0,0 +1,8 @@
+
+void readFile(){
+
+}
+
+void saveFile(){
+    
+}
