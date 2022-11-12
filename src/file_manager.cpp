7b5033eecc753fa83338a0ebe483b8fea81921b2
Gabriel <gc.novaes@outlook.com>
2022-11-11 22:07:29 -0300

    adiciona funcao de split

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -6,19 +6,47 @@ void readFile(LinkedList *ll){
 	f.open(FILE_PATH, std::ios::in); 
 	
 	std::string line;
-	
+	std::string* stringArray = new std::string[9];
 
-	if (f.is_open())
-  	{
+	if (f.is_open()){
 	    while(getline(f, line)){
-	    	std::cout << "linha: " << line << "" << std::endl; 
+	    	
+	    	substring(line, stringArray);
+	    	
+	    	std::cout << "linha: ";	    	
+	    	for(int i = 0; i < 9; i++){
+	    		std::cout << stringArray[i] << " ";
+			}
+			
+			std::cout << std::endl;
 	    }
 	f.close();
 	}
 	
+	delete stringArray;
+	stringArray = nullptr;
 }
 
 
 void saveFile(){
     
+}
+
+// retorna um array com as 9 colunas do csv
+void substring(std::string s, std::string* v){
+	int j = 0;
+	std::string aux;
+	
+	for(int i = 0; i < 9; i++){
+
+		while (s[j] != ';' && j < s.size()){
+			aux.push_back(s[j]);
+			j++;
+		}
+		
+		v[i] = aux;
+		aux = "";
+		j++;
+	}
+
 }
--- src/file_manager.h
+++ src/file_manager.h
@@ -2,14 +2,18 @@
 #define __FILE_MANAGER_H__
 
 #include "linked_list.h"
-#include <string>
+#include <stdio.h>
+#include <string.h>
+
 #include <fstream>
 #include <iostream>
+#include "books.h"
 
 const int MAX = 80;
 const std::string FILE_PATH = "bibliografia.csv";
 
 void readFile(LinkedList *ll);
 void saveFile();
+void substring(std::string s, std::string* v);
 
 #endif
